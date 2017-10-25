ec4a7a7c3ab16327eca7ec76541e27c10c396832
Jannis R <mail@jannisr.de>
2017-10-25 02:58:07 +0200

    WIP

---
+++ generate.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+
+const html = fs.readFileSync(path.join(__dirname, 'decrypt.html'), {encoding: 'utf8'})
+const js = fs.readFileSync(path.join(__dirname, 'decrypt.js'), {encoding: 'utf8'})
+
+const generate = (nonce, encrypted) => {
+	if (!Buffer.isBuffer(nonce)) throw new Error('nonce must be a Buffer.')
+	if (!Buffer.isBuffer(encrypted)) throw new Error('encrypted must be a Buffer.')
+
+	return html
+	.replace('{{nonce}}', nonce.toString('hex'))
+	.replace('{{encrypted}}', encrypted.toString('hex'))
+	.replace('{{js}}', js)
+}
+
+module.exports = generate
